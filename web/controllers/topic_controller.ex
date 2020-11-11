c9f95ef14f34c1ca78cd7ddc22393dd65a0710ae
justinakoh <justina.y.koh@gmail.com>
2020-11-12 08:52:39 +1300

    Update code to complete s10 p79
    This change allows things to be inserted into the database from the form but it shows and error on the scnree

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -5,8 +5,22 @@ defmodule Discuss.TopicController do
 
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    
+
     render conn, "new.html", changeset: changeset
+  end
+
+  # When things are being posted: here we want to handle both cases where things are successful and also where there is an error
+  def create(conn, %{"topic" => topic}) do
+    # this is the change that we want to make to our database:as we are creating something new we pass in an empty struct i.e. %Topic{}
+    # If we were going to edit something we would pass in a struct with a title and id
+    changeset = Topic.changeset(%Topic{}, topic)
+
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
+
   end
 
 end
