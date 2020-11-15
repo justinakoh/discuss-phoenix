ad244020b5b00f1886760974c51c123bca598962
justinakoh <justina.y.koh@gmail.com>
2020-11-15 16:48:20 +1300

    Update code so the authentication is working

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,12 +1,47 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
-
-  # New Version
-  # pipeline :browser do
-  #   plug Ueberauth
-
   plug Ueberauth
+  alias Discuss.User
 
-  def callback(conn, params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    IO.inspect(params)
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
   end
+
+  #A private function that signs a user in based on cookies and user id
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+
+  end
+
+  #A private function that inserts or updates a user record based on his email
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
+  #Function to delete any user information in the current session
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
 end
